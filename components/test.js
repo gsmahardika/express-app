83c608c6de7585d20a861b4ba82c8931fcfc97ac
Gs Mahardika <gs.mahardika@gmail.com>
2018-08-25 21:35:32 +0800

    Update mysql example

--- components/test.js
+++ components/test.js
@@ -2,18 +2,37 @@ const express = require('express')
 const mysql = require('../services/mysql')
 const router = express.Router()
 
-router.get('/user', (req, res, next) => {
-  mysql.query('SELECT * FROM test', (err, rows) => {
+let getInitialData = (res) => {
+  mysql.query('SELECT * FROM test', (err, results) => {
     if (err) {
       throw err
     }
 
-    res.json({
-      status: true,
-      message: 'Hello world from /test/user!',
-      data: rows
-    })
+    getExtendedData(res, results)
   })
+}
+
+let getExtendedData = (res, data) => {
+  mysql.query('SELECT * FROM test', (err, results) => {
+    if (err) {
+      throw err
+    }
+
+    returnResponse(res, data, results)
+  })
+}
+
+let returnResponse = (res, initialData, extendedData) => {
+  res.json({
+    status: true,
+    message: 'Hello world from /test/user!',
+    initialData: initialData,
+    extendedData: extendedData
+  })
+}
+
+router.get('/user', (req, res, next) => {
+  getInitialData(res)
 })
 
 module.exports = router
